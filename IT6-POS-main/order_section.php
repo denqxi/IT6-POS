5adf0957c2ed45fc7c649e5fe0e4c244be51475c
Den <168108755+denqxi@users.noreply.github.com>
2025-02-27 15:43:42 +0800

    Delete IT6-POS-main directory

--- IT6-POS-main/order_section.php
+++
@@ -1,6 +0,0 @@
-<div class="content-section" id="order-section">
-    <div class="ms-3">
-        <h3>Orders Section</h3>
-        <p>Order history and current orders go here.</p>
-    </div>
-</div>
